31031616470d138db1c3f686a63f3b1bcf2af3af
Matheussgsilva <matheussgsilva@hotmail.com>
2021-11-12 19:20:45 -0300

    Create the Layout component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {Fragment} from 'react'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
+import Container from './components/layout/Container';
+
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/company" element={<Company/>}/>
-        <Route exact path="/contact" element={<Contact/>}/>
-        <Route exact path="/newproject" element={<NewProject/>}/>
+        <Fragment customClass="min_height">
+          <Route path="/" element={<Home/>}/>
+          <Route path="/company" element={<Company/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/newproject" element={<NewProject/>}/>
+        </Fragment>
       </Routes>
     </Router>
   );
