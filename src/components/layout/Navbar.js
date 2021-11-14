7e79e74e7a3f335213e8955b19206de59efd88d2
Matheussgsilva <matheussgsilva@hotmail.com>
2021-11-14 11:22:12 -0300

    Create the navbar list and Navbar style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,15 +7,22 @@ import logo from '../../image/costs_logo.png'
 
 function Navbar() {
     return (
-    <nav>
+    <nav className={styles.navbar}>
         <Container>
             <Link to="/">
                 <img src={logo} alt="Imagem moeda"/>
             </Link>
-            <Link to="/">Home</Link>
-            <Link to="/company">Empresa</Link>
-            <Link to="/contact">Contato</Link>
-            <Link to="/newproject">Novo Projeto</Link>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <Link to="/">Home</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to="/company">Empresa</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to="/contact">Contato</Link>
+                </li>
+            </ul>       
         </Container>
     </nav>
     )
