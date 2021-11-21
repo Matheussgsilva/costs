609ba0edaf591e28e2f8b70b8ebc302cc1201ea4
Matheussgsilva <matheussgsilva@hotmail.com>
2021-11-21 19:04:38 -0300

    Add service form container

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,6 +14,7 @@ function Project() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -33,6 +34,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -58,6 +61,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
     
     return (
@@ -87,6 +94,19 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                                <h2>Adicione um serviço:</h2>
+                                <button className={styles.btn} onClick={toggleServiceForm}>
+                                    {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                                </button>
+                                <div className={styles.project_info}>
+                                    {showServiceForm && <div>Formulário de Serviço</div>}
+                                </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass={"start"}>
+                            <p>Itens de Serviços</p>
+                        </Container>
                     </Container>
                 </div>
                 ):(<Loading />))
